1f26e64d1915a301583812269175ef59b0f60f91
other-ones <tuk101234@gmail.com>
2024-07-29 00:09:12 -0400

    update

--- src_english/generate_batch_english_cfg_schedule_final.py
+++ src_english/generate_batch_english_cfg_schedule_final.py
@@ -508,6 +508,7 @@ def main(args):
                 print(accelerator.num_processes ,'num_processes')
                 print('saved at', sample_dir)
                 print('STEP: {}/{} ETA:{:.4f}'.format(step+1,len(test_dataloader),(moving_avg*num_remaining)/60))
+                st=time.time()
                 count+=1
     if accelerator.is_main_process:   
         print(count,'ended')
